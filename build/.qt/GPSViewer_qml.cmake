b8a23af781687a00afa81c3dcdec6a6d30ae3694
JaswanthPalla149 <pallajaswanthroyal@gmail.com>
2025-06-29 11:11:06 +0530

    Correcting .gitignore

--- build/.qt/GPSViewer_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "GPSViewer")
-set(working_dir "C:/Users/Jaswanth/Desktop/gps_qt_server")
-set(src_and_dest_list
-    "C:/Users/Jaswanth/Desktop/gps_qt_server/qml/Main.qml"
-    "/GPSViewer/qml/Main.qml"
-
-)
-set(timestamp_file "C:/Users/Jaswanth/Desktop/gps_qt_server/build/.qt/GPSViewer_qml.txt")
--- build/CMakeFiles/GPSViewer_qmllint_module.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/GPSViewer_qmllint_module"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/GPSViewer_qmllint_module.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
